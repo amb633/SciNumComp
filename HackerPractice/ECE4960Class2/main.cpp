aa5685e999220a45f863cfd748adb7242b62cda1
Ariana Bruno <arianabruno@arianas-mbp-2.home>
2018-02-04 22:52:25 -0500

    further updates to the hacker practice

--- HackerPractice/ECE4960Class2/main.cpp
+++ HackerPractice/ECE4960Class2/main.cpp
@@ -20,16 +20,19 @@ int demo_int_overflow(int N){
 void demo_quadratic1_precision( double a ){
     double b = pow(10,3);
     double c = pow(10,3);
-    std::cout.precision(9);
+    std::cout.precision(5);
     std::cout << std::scientific;
+    
     std::cout << "a: " << a << ", b: " << b << ", c: " << c << std::endl;
     std::cout << "-b + sqrt(b^2 - 4*a*c): " << (-b) + sqrt(pow(b,2) - 4*a*c) << std::endl;
-    std::cout << "-b  sqrt(b^2 - 4*a*c): " << (-b) - sqrt(pow(b,2) - 4*a*c) << std::endl;
+    std::cout << "-b - sqrt(b^2 - 4*a*c): " << (-b) - sqrt(pow(b,2) - 4*a*c) << std::endl;
     std::cout << "2a: " << (2*a) << std::endl;
+    std::cout << std::endl;
     
     
-    double x1 = ((-b) + sqrt(pow(b,2) - (4*a*c)))/(2*a);
-    double x2 = ((-b) - sqrt(pow(b,2) - (4*a*c)))/(2*a);
+    
+    double x1 = ((-b) - sqrt(pow(b,2) - (4*a*c)))/(2*a);
+    double x2 = ((-b) + sqrt(pow(b,2) - (4*a*c)))/(2*a);
     
     std::cout << "Quadratic method 1 for a= " << a << " is: x1= " << x1 << " and x2= " << x2 << std::endl;
 }
@@ -38,35 +41,45 @@ void demo_quadratic1_precision( double a ){
 void demo_quadratic2_precision( double a ){
     double b = pow(10,3);
     double c = pow(10,3);
-    std::cout.precision(9);
+    std::cout.precision(5);
     std::cout << std::scientific;
+    
     std::cout << "a: " << a << ", b: " << b << ", c: " << c << std::endl;
     std::cout << "-b + sqrt(b^2 - 4*a*c): " << (-b) + sqrt(pow(b,2) - 4*a*c) << std::endl;
-    std::cout << "-b  sqrt(b^2 - 4*a*c): " << (-b) - sqrt(pow(b,2) - 4*a*c) << std::endl;
+    std::cout << "-b - sqrt(b^2 - 4*a*c): " << (-b) - sqrt(pow(b,2) - 4*a*c) << std::endl;
     std::cout << "2c: " << (2*a) << std::endl;
+    std::cout << std::endl;
+    
     
     double x1 = (2*c)/(-b + sqrt(pow(b,2) - (4*a*c)));
     double x2 = (2*c)/(-b - sqrt(pow(b,2) - (4*a*c)));
-    
+
     std::cout << "Quadratic method 2 for a= " << a << " is: x1= " << x1 << " and x2= " << x2 << std::endl;
+    std::cout << std::endl;
+    
+    double est_x1 = -(b/a) + (c/b);
+    double est_x2 = -c/b;
+    std::cout << "Quadratic estimate for a= " << a << " is: x1_est= " << est_x1 << " and x2_est= " << est_x2 << std::endl;
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    //std::cout << "Hello, World!\n" << std::endl;
+
     std::cout.precision(9);
-    
-    
+
+
     //demonstrates that the 'int' will overflow once the length of the result is greater than 32 bits
     int int_result = demo_int_overflow(20);
     std::cout << "The overflow demo for 'int' is: "<< int_result << std::endl;
     std::cout << std::endl;
-    
+
+    //demonstrates that we get the same results from all 3 methods
     demo_quadratic1_precision(pow(10,-5));
-    std::cout << std::endl;
     demo_quadratic2_precision(pow(10,-5));
     std::cout << std::endl;
     
+    //demonstrates how the first method we can get the larger x result
+    // where as the second method we get the smaller result
+    // lastly the estimated results are able to provide the small and large result for x
     std::cout << "Now using a as 10^-20 " << std::endl;
     demo_quadratic1_precision(pow(10,-20));
     std::cout << std::endl;
--- HackerPractice/ECE4960Class3/main.cpp
+++ HackerPractice/ECE4960Class3/main.cpp
@@ -9,11 +9,13 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>  //for the std::setprecision function
+#include <limits>  //for the infinity();
 
 
-void isPositiveZero( double x){
+void isPositiveZero( float x){
     std::string answer;
-    if (x > 0.0){
+    float zero_pos = +0.0;
+    if (std::signbit(x) == 0){
         answer = "True";
         std::cout << answer << ", " << x << " is positive" << std::endl;
     } else {
@@ -22,12 +24,12 @@ void isPositiveZero( double x){
     }
 }
 
-void isNegativeZero( double x){
+void isNegativeZero( float x){
     std::string answer;
-    if (x < 0.0){
+    if (std::signbit(x) == 1){
         answer = "True";
         std::cout << answer << ", " << x << " is negative" << std::endl;
-    } else {
+    }else {
         answer = "False";
         std::cout << answer << ", " << x << " is not negative" << std::endl;
     }
@@ -36,33 +38,39 @@ void isNegativeZero( double x){
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    /*
-    //Hacker Pactice Part 1
+    
+    //Hacker Pactice 2.2
     // Observe the exception handling on your platform
     double x = 0.0, y = 0.0, doubleResult1, doubleResult2;
     doubleResult1 = 1/x;
     doubleResult2 = y/x;
-    
+
     //should return NaN and INF
     std::cout << doubleResult1 << ", " << doubleResult2 << std::endl;
-     */
+
+//    //returns an error from the compiler because INF cannot be represented by integer
+//    long m=0, n=0, intResult1, intResult2;
+//    intResult1 = 1/m;
+//    intResult2=m/n;
+//
+//    std::cout << intResult1 << ", " << intResult2 << std::endl;
+
     
-    /*
-     long m=0, n=0, intResult1, intResult2;
-    intResult1 = 1/m;
-    intResult2=m/n;
-    //returns an error from the compiler because INF cannot be represented by integer
-    std::cout << intResult1 << ", " << intResult2 << std::endl;
-     */
-    
-    /*
-    //Hacker Practice 2
+    //Hacker Practice 2.3
     isPositiveZero(1.0);
     isPositiveZero(-1.0);
     isPositiveZero(__DBL_MAX__);
     isPositiveZero(-1.0*__DBL_MAX__);
     isPositiveZero(__DBL_MIN__);
     isPositiveZero(-1.0*__DBL_MIN__);
+    float INF = std::numeric_limits<float>::infinity();
+    isPositiveZero(INF);
+    isPositiveZero(-1.0*INF);
+    isPositiveZero(1.0/0.0);
+    isPositiveZero(-1.0/0.0);
+    isPositiveZero(+0.0);
+    isPositiveZero(-0.0);
+    std::cout << std::endl;
     
     isNegativeZero(1.0);
     isNegativeZero(-1.0);
@@ -70,21 +78,37 @@ int main(int argc, const char * argv[]) {
     isNegativeZero(-1.0*__DBL_MAX__);
     isNegativeZero(__DBL_MIN__);
     isNegativeZero(-1.0*__DBL_MIN__);
-     */
+    isNegativeZero(INF);
+    isNegativeZero(-1.0*INF);
+    isNegativeZero(1.0/0.0);
+    isNegativeZero(-1.0/0.0);
+    isNegativeZero(+0.0);
+    isNegativeZero(-0.0);
+    std::cout << std::endl;
     
-    //Hacker Practice 3
-    double x = 1.234567890123456;
+    
+    //Hacker Practice 2.4
+    //soft landing of a flowing point number
+    double a = 1.234567890123456;
     int i = 1;
-    
-    x *= pow(10,-307);
-    
+
+    a *= pow(10,-307);
+
     for (i=1; i<20; i++) {
-        x /= 10.0;
-        std::cout << std::setprecision(32) << x << std::endl;
+        a /= 10.0;
+        std::cout << std::setprecision(16) << a << std::endl;
     }
-    
-    
-    
+
+    //another approach to show the percentage change between the previous
+    //and new point precision error as we approach underflow
+    double af = 1.234567890123456;
+    af *= pow(10,-307);
+    for (i=1; i<20; i++){
+        double af_new = af/10.0;
+        double diff = (af - (af_new*10))/(af);
+        std::cout << " " << std::setprecision(32) << diff << std::endl;
+        af = af_new;
+    }
     
     
     
